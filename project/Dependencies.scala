92e63e66686784ddd2e53d3cd02624aa7ffeefbe
Jon <dyercode@users.noreply.github.com>
2021-02-06 15:49:10 -0500

    how long was that dep not used? oof.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,6 @@ object Dependencies {
 
   // Libraries
   val scalaTest = "org.scalatest" %% "scalatest" % "3.2.3"
-  val parserCombinators = "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2"
-  val scalacheck = "org.scalacheck" %% "scalacheck" % "1.14.3"
   val simulacrum = "org.typelevel" %% "simulacrum" % "1.0.0"
 
   // Projects
@@ -17,8 +15,7 @@ object Dependencies {
   val evercraftFrDependencies: Seq[ModuleID] =
     Seq(simulacrum, scalaTest % Test, scalacheck % Test)
   val fibonacciDependencies: Seq[ModuleID] = Seq(scalaTest % Test)
-  val bowlingDependencies: Seq[ModuleID] =
-    Seq(parserCombinators, scalaTest % Test)
+  val bowlingDependencies: Seq[ModuleID] = Seq(scalaTest % Test)
   val leapDependencies: Seq[ModuleID] = Seq(scalaTest % Test)
   val chopDependencies: Seq[ModuleID] = Seq(scalaTest % Test)
   val triangleDependencies: Seq[ModuleID] = Seq(scalaTest % Test)
